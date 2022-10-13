9b77620a40242bf20475ed07e547162e20640245
guitartarotti <henrique.5gzt@gmail.com>
2022-10-13 17:43:54 -0300

    Add documentation with Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 import { simulationRoutes } from '@routes/simulations.routes'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/simulations', simulationRoutes)
 
